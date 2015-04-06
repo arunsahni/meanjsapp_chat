cec267e0a2760fe1e521efb2238449638320a397
Pankaj Dewangan <pankaj.dewangan@sumasoft.net>
2015-04-06 17:33:20 +0530

    #7, Implementing HighChart.

---
+++ public/modules/core/directives/sumahighchart.client.directive.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('core').directive('sumahighchart', function () {
+	return {
+		restrict: 'E',
+		template: '<div></div>',
+		scope: {
+			chartData: "=value",
+			chartObj: "=?"
+		},
+		transclude: true,
+		replace: true,
+		link: function($scope, $element, $attrs) {
+
+			//Update when charts data changes
+			$scope.$watch('chartData', function(value) {
+				if (!value)
+					return;
+				$scope.chartData.chart = $scope.chartData.chart || {};
+				$scope.chartData.chart.renderTo = $scope.chartData.chart.renderTo || $element[0];
+				$scope.chartObj = new Highcharts.Chart($scope.chartData);
+			});
+		}
+	};
+});
