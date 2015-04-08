9935bca9835f1e0a413a7d41274f598dcc89d09e
Pankaj Dewangan <pankaj.dewangan@sumasoft.net>
2015-04-08 16:48:54 +0530

    Remote DB for mean app.

--- config/env/production.js
+++ config/env/production.js
@@ -2,7 +2,8 @@
 
 module.exports = {
 	//db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/meanjsapp',
-	db: 'mongodb://localhost/meanjsapp-dev',
+	//db: 'mongodb://localhost/meanjsapp-dev',
+	db: 'mongodb://suma:sumapass@ds061671.mongolab.com:61671/meanjsapp-dev', // Our server database
 	assets: {
 		lib: {
 			css: [
