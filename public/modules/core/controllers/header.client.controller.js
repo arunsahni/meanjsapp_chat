3faf41110f25fdcede54d33f80b12d6edb95acdb
bharat tambe <bharat.tambe@sumasoft.net>
2015-04-10 17:19:51 +0530

    button toggle

--- public/modules/core/controllers/header.client.controller.js
+++ public/modules/core/controllers/header.client.controller.js
@@ -6,12 +6,13 @@ angular.module('core').controller('HeaderController', ['$scope', 'Authentication
 		$scope.isCollapsed = false;
 		$scope.menu = Menus.getMenu('topbar');
 		$scope.imgPath = Authentication.user._id + '.png';
-
+		$scope.isActive = true;
 		$scope.toggleCollapsibleMenu = function() {
 			$scope.isCollapsed = !$scope.isCollapsed;
 		};
 		$scope.changeLanguage = function (langKey) {
 			$translate.use(langKey);
+			$scope.isActive = !$scope.isActive;
 		};
 		// Collapsing the menu after navigation
 		$scope.$on('$stateChangeSuccess', function() {
