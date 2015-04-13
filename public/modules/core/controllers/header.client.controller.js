d1694659061709aebf1a2f4c0e2406cf1f816fa2
paragwaghela <parag@highground.com>
2015-04-13 12:28:41 +0530

    #15  After logout user should not able to click on back button or we need to handle it programmatically.

--- public/modules/core/controllers/header.client.controller.js
+++ public/modules/core/controllers/header.client.controller.js
@@ -1,7 +1,8 @@
 'use strict';
 
-angular.module('core').controller('HeaderController', ['$scope', 'Authentication', 'Menus','toastr', 'PusherService','$translate',
-	function($scope, Authentication, Menus, toastr, PusherService, $translate) {
+angular.module('core').controller('HeaderController', ['$scope', 'Authentication', 'Menus','toastr', 'PusherService','$translate','$location',
+	function($scope, Authentication, Menus, toastr, PusherService, $translate,$location) {
+
 		$scope.authentication = Authentication;
 		$scope.isCollapsed = false;
 		$scope.menu = Menus.getMenu('topbar');
@@ -17,6 +18,14 @@ angular.module('core').controller('HeaderController', ['$scope', 'Authentication
 		};
 		// Collapsing the menu after navigation
 		$scope.$on('$stateChangeSuccess', function() {
+			if (!$scope.authentication.user ) {
+				if($location.path() === '/signup') {
+					$location.path('/signup');
+				}
+				else{
+					$location.path('/#!');
+				}
+			}
 			$scope.isCollapsed = false;
 			$scope.imgPath = 'https://s3.amazonaws.com/sumacrm/avatars/' + Authentication.user._id;
 		});
