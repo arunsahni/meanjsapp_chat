81167304909da7e8aeee15d507d286b4b5c75b02
Pankaj Dewangan <pankaj.dewangan@sumasoft.net>
2015-04-17 13:27:35 +0530

    build.

--- public/modules/articles/config/articles.client.config.js
+++ public/modules/articles/config/articles.client.config.js
@@ -4,7 +4,7 @@
 angular.module('articles').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		Menus.addMenuItem('topbar', 'acc.sub.das','dropdown', '/#!(/dashboard)');
+		Menus.addMenuItem('topbar', 'acc.sub.das', 'dashboard', '/#!(/dashboard)');
 		Menus.addMenuItem('topbar', 'acc.sub.art', 'articles', 'dropdown', '/articles(/create)?');
 		Menus.addSubMenuItem('topbar', 'articles', 'acc.sub.item', 'articles');
 		Menus.addSubMenuItem('topbar', 'articles', 'acc.sub.new', 'articles/create');
