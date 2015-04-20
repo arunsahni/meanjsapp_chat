01a48c255ed33994a78f8ece5100cac08156c100
arun sahni <arun_sahni@ymail.com>
2015-04-20 19:35:11 +0530

    checked role for route access

--- public/modules/admins/config/admins.client.routes.js
+++ public/modules/admins/config/admins.client.routes.js
@@ -1,13 +1,33 @@
 'use strict';
 
+
+
 //Setting up route
 angular.module('admins').config(['$stateProvider',
 	function($stateProvider) {
+
+		var checkRole = function($q, $timeout, $location, Authentication) {
+			// Initialize a new promise
+			var deferred = $q.defer();
+			var authentication = Authentication.user.roles[0];
+			console.log(authentication);
+			if (authentication === 'admin')
+				$timeout(deferred.resolve);
+			else {
+					$timeout(deferred.reject);
+					$location.url('/dashboard');
+				}
+			return deferred.promise;
+		};
+
 		// Admins state routing
 		$stateProvider.
 			state('userlist', {
 			url: '/userlist',
-			templateUrl: 'modules/admins/views/userList.html'
+			templateUrl: 'modules/admins/views/userList.html',
+			resolve: {
+				checkRole: checkRole
+			}
 			});
 	}
 ]);
