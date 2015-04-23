6449ff47cc453c21d56276d415ba3124d6ba9b29
Naitik Soni <naitik.soni@sumasoft.net>
2015-04-23 13:02:02 +0530

    #28, Task Management module, Schema and  create task page.

---
+++ app/models/task.server.model.js
@@ -0,0 +1,46 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * Task Schema
+ */
+var TaskSchema = new Schema({
+	name: {
+		type: String,
+		default: '',
+		required: 'Please fill Task name',
+		trim: true
+	},
+	createdDate: {
+		type: Date,
+		default: Date.now
+	},
+    description: {
+        type: String,
+        default: ''
+    },
+    priority: {
+        type: String,
+        default: ''
+    },
+    assignees: [{
+        type: Schema.ObjectId,
+        ref: 'User',
+        required: 'Please fill assignee'
+    }],
+    deadline: {
+        type: Date,
+        required: 'Please fill dead line date'
+    },
+	createdBy: {
+		type: Schema.ObjectId,
+		ref: 'User'
+	}
+});
+
+mongoose.model('Task', TaskSchema);
---
+++ public/modules/tasks/controllers/tasks.client.controller.js
@@ -0,0 +1,81 @@
+'use strict';
+
+// Tasks controller
+angular.module('tasks').controller('TasksController', ['$scope', '$stateParams', '$location', 'Authentication', 'Tasks',
+	function($scope, $stateParams, $location, Authentication, Tasks) {
+		$scope.authentication = Authentication;
+        $scope.task = {};
+
+		// Create new Task
+		$scope.createTask = function() {
+			// Create new Task object
+			var task = new Tasks ({
+				name: this.name
+			});
+
+			// Redirect after save
+			task.$save(function(response) {
+				$location.path('tasks/' + response._id);
+
+				// Clear form fields
+				$scope.name = '';
+			}, function(errorResponse) {
+				$scope.error = errorResponse.data.message;
+			});
+		};
+
+        $scope.autoCompleteData = {
+            EntityName: ['User'],
+            Projection: ['firstName', '_id', 'isImage'],
+            MatchField: 'firstName'
+        };
+
+        $scope.$watchCollection('assignees',function(val){
+            if ($scope.assignees && $scope.assignees.length) {
+                var userIds = $scope.assignees.map(function (user) {
+                    return user._id;
+                });
+                $scope.task.assignees = userIds;
+            }
+        });
+		// Remove existing Task
+		$scope.remove = function(task) {
+			if ( task ) { 
+				task.$remove();
+
+				for (var i in $scope.tasks) {
+					if ($scope.tasks [i] === task) {
+						$scope.tasks.splice(i, 1);
+					}
+				}
+			} else {
+				$scope.task.$remove(function() {
+					$location.path('tasks');
+				});
+			}
+		};
+
+		// Update existing Task
+		$scope.update = function() {
+			var task = $scope.task;
+
+			task.$update(function() {
+				$location.path('tasks/' + task._id);
+			}, function(errorResponse) {
+				$scope.error = errorResponse.data.message;
+			});
+		};
+
+		// Find a list of Tasks
+		$scope.find = function() {
+			$scope.tasks = Tasks.query();
+		};
+
+		// Find existing Task
+		$scope.findOne = function() {
+			$scope.task = Tasks.get({ 
+				taskId: $stateParams.taskId
+			});
+		};
+	}
+]);
