5bb1cc420bd62985f4dcd251cd298eee9bb3138f
arun sahni <arun_sahni@ymail.com>
2015-04-24 13:46:27 +0530

    #access mgt minor changes

--- public/modules/admins/config/admins.client.config.js
+++ public/modules/admins/config/admins.client.config.js
@@ -5,9 +5,8 @@ angular.module('admins').run(['Menus', 'Authentication',
 	function(Menus, Authentication) {
 		// Set top bar menu items
 
-		Menus.addMenuItem('topbar', 'admin.admin', 'admins', 'dropdown', '/admins(/create)?', false, ['admin', 'superadmin']);
-		Menus.addSubMenuItem('topbar', 'admins', 'admin.role', 'userlist','userlist', false, ['admin', 'superadmin']);
-		Menus.addSubMenuItem('topbar', 'admins', 'admin.access', 'featuresAccess','featuresAccess', false, ['admin']);
-
+		Menus.addMenuItem('topbar', 'admin.adm', 'admins', 'dropdown', '/admins(/create)?', false, ['admin', 'superadmin']);
+		Menus.addSubMenuItem('topbar', 'admins', 'admin.rmgt', 'userlist','userlist', false, ['admin', 'superadmin']);
+		Menus.addSubMenuItem('topbar', 'admins', 'admin.accmgt', 'featuresAccess','featuresAccess', false, ['admin']);
 	}
 ]);
