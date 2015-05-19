0dbf428e0ff5a906b4884d362de77e829f686bd2
paragwaghela <parag@highground.com>
2015-05-19 17:02:38 +0530

    ChatSchema code refactor (remove count query).

--- app/controllers/core.server.controller.js
+++ app/controllers/core.server.controller.js
@@ -30,22 +30,15 @@ exports.chatSave = function(req,res) {
 };
 
 exports.chatFind = function(req,res){
-	Chat.count({},function(err,count){
-		if(err) {
+	Chat.find({}).sort({'chatDate':-1}).limit(10).populate('sender reciever').exec(function (err, chats) {
+		if (err) {
 			return res.status(400).send({
 				message: errorHandler.getErrorMessage(err)
 			});
-		}
-		else {
-			Chat.find({}).skip(count - 10).populate('sender reciever').exec(function (err, chats) {
-				if (err) {
-					return res.status(400).send({
-						message: errorHandler.getErrorMessage(err)
-					});
-				} else {
-					res.json(chats);
-				}
-			});
+		} else {
+			res.json(chats.reverse());
 		}
 	});
+
+
 };
